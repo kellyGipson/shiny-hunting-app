949e32121c3ab8bde027b219e89fc6f488da6e65
Kelly Gipson <thomaskellygipson@gmail.com>
2022-09-24 16:55:14 -0400

    chore(): move selection components to proper subfolder

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,9 +1,9 @@
 import { CurrentHuntComponent } from "./current-hunt/current-hunt.component";
-import { GameSelectComponent } from "./game-select/game-select.component";
-import { MethodSelectComponent } from "./method-select/method-select.component";
+import { GameSelectComponent } from "./new-hunt/game-select/game-select.component";
+import { MethodSelectComponent } from "./new-hunt/method-select/method-select.component";
 import { NavComponent } from "./nav/nav.component";
 import { NewHuntComponent } from "./new-hunt/new-hunt.component";
-import { PokemonSelectComponent } from "./pokemon-select/pokemon-select.component";
+import { PokemonSelectComponent } from "./new-hunt/pokemon-select/pokemon-select.component";
 import { PokemonComponent } from "./prev-hunt/prev-hunt.component";
 
 export const APP_COMPONENTS = [
