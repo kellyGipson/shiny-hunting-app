903daac92484b0d80d81674ef322b5a42bb8d62a
Kelly Gipson <thomaskellygipson@gmail.com>
2022-09-25 00:36:31 -0400

    feat(): add selected hunt component

---
+++ src/app/core/home/selected-hunt/selected-hunt.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'app-selected-hunt',
+  templateUrl: './selected-hunt.component.html',
+  styleUrls: ['./selected-hunt.component.scss']
+})
+export class SelectedHuntComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -6,6 +6,7 @@ import { NewHuntComponent } from "./new-hunt/new-hunt.component";
 import { PokemonSelectComponent } from "./new-hunt/pokemon-select/pokemon-select.component";
 import { PokemonComponent } from "./prev-hunt/prev-hunt.component";
 import { EditHuntComponent } from "./home/edit-hunt/edit-hunt.component";
+import { SelectedHuntComponent } from "./home/selected-hunt/selected-hunt.component";
 
 export const APP_COMPONENTS = [
   CurrentHuntComponent,
@@ -16,4 +17,5 @@ export const APP_COMPONENTS = [
   GameSelectComponent,
   MethodSelectComponent,
   EditHuntComponent,
+  SelectedHuntComponent,
 ]
