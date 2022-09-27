de0af46f273673b82c631df3676777ee433a5aa9
Kelly Gipson <thomaskellygipson@gmail.com>
2022-09-26 22:07:32 -0400

    chore():

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -4,8 +4,8 @@ import { NavComponent } from "./nav/nav.component";
 import { NewHuntComponent } from "./new-hunt/new-hunt.component";
 import { PokemonSelectComponent } from "./new-hunt/pokemon-select/pokemon-select.component";
 import { PokemonComponent } from "./prev-hunt/prev-hunt.component";
-import { EditHuntComponent } from "./home/edit-hunt/edit-hunt.component";
-import { SelectedHuntComponent } from "./home/selected-hunt/selected-hunt.component";
+import { EditHuntComponent } from "./edit-hunt/edit-hunt.component";
+import { SelectedHuntComponent } from "./selected-hunt/selected-hunt.component";
 
 export const APP_COMPONENTS = [
   NavComponent,
