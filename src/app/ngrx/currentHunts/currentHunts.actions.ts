ff6de689cdbb49b7f95742d6e8d9a260cc94969c
Kelly Gipson <thomaskellygipson@gmail.com>
2023-07-23 11:41:04 -0400

    chore: cleaned up reducers

--- src/app/ngrx/currentHunts/currentHunts.actions.ts
+++ src/app/ngrx/currentHunts/currentHunts.actions.ts
@@ -1,36 +1,29 @@
 import { createAction, props } from "@ngrx/store"
 import { Hunt } from "src/app/types/Hunts.types";
 
-export const currentHuntsActionsType = {
-  ADD_CURRENT_HUNTS_OPEN_ACTION: 'ADD_CURRENT_HUNTS_OPEN_ACTION',
-  DELETE_CURRENT_HUNTS_OPEN_ACTION: 'DELETE_CURRENT_HUNTS_OPEN_ACTION',
-  UPDATE_CURRENT_HUNTS_OPEN_ACTION: 'UPDATE_CURRENT_HUNTS_OPEN_ACTION',
-  SET_CURRENT_HUNTS_OPEN_ACTION: 'SET_CURRENT_HUNTS_OPEN_ACTION',
-}
-
 export const addCurrentHuntsAction = createAction(
-  currentHuntsActionsType.ADD_CURRENT_HUNTS_OPEN_ACTION,
-  props<Hunt>()
+  'ADD_CURRENT_HUNTS_OPEN_ACTION',
+  props<{ hunt: Hunt }>(),
 );
 
 export const deleteCurrentHuntsAction = createAction(
-  currentHuntsActionsType.DELETE_CURRENT_HUNTS_OPEN_ACTION,
-  props<Hunt>(),
+  'DELETE_CURRENT_HUNTS_OPEN_ACTION',
+  props<{ hunt: Hunt }>(),
 );
 
 export const updateCurrentHuntsAction = createAction(
-  currentHuntsActionsType.UPDATE_CURRENT_HUNTS_OPEN_ACTION,
-  props<Hunt>(),
+  'UPDATE_CURRENT_HUNTS_OPEN_ACTION',
+  props<{ hunt: Hunt }>(),
 );
 
 export const setCurrentHuntsAction = createAction(
-  currentHuntsActionsType.SET_CURRENT_HUNTS_OPEN_ACTION,
+  'SET_CURRENT_HUNTS_OPEN_ACTION',
   props<{ list: Hunt[] }>(),
 );
 
 export const currentHuntsActionsContainer = {
-  addCurrentHuntsAction: addCurrentHuntsAction,
-  deleteCurrentHuntsAction: deleteCurrentHuntsAction,
-  updateCurrentHuntsAction: updateCurrentHuntsAction,
-  setCurrentHuntsAction: setCurrentHuntsAction,
+  addCurrentHuntsAction,
+  deleteCurrentHuntsAction,
+  updateCurrentHuntsAction,
+  setCurrentHuntsAction,
 }
